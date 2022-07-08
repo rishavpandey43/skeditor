026f8130811c856f3059e3ffd10e06438fd43576
Rishav Pandey <27219302+rishavpandey43@users.noreply.github.com>
2022-07-08 15:43:04 +0530

    :sparkles: add setup for demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { SkEditor } from '@skuad/skeditor';
 
-function App() {
+import { _templateData, _globalPlaceholders } from './constants';
+
+const App = () => {
+  const [templateData, setTemplateData] = useState(_templateData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SkEditor
+      templateData={templateData}
+      globalPlaceholders={_globalPlaceholders}
+      onChange={(value: any) => setTemplateData(value)}
+    />
   );
-}
+};
 
 export default App;
